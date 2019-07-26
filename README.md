e5b86d74e6bfa93c0ea30b5957240bdbe72899d6
Sen Khvorov <khvorov45@gmail.com>
2019-07-27 08:52:31 +1000

    Added content

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# CmdParser
- Reads command line input
+# Command line parsing
+
+ Reads command line input of the form:
+
+ ~~~
+python path/to/script.py args -o option1 -i option2
+ ~~~
+
+and returns a dictionary of options and a list of non-options.
---
+++ setup.py
@@ -0,0 +1,22 @@
+# pylint: disable=missing-docstring
+import setuptools
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setuptools.setup(
+    name="cmdparser-khvorov45",
+    version="0.0.1",
+    author="Arseniy Khvorov",
+    author_email="khvorov45@gmail.com",
+    description="Parses command line input",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/khvorov45/cdmparser",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)

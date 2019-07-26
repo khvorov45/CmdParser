26d4a5edab55b78d15c4919797b5690275fa0503
Sen Khvorov <khvorov45@gmail.com>
2019-07-27 09:11:41 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,9 +14,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/khvorov45/cdmparser",
     packages=setuptools.find_packages(),
+    license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Operating System :: OS Independent"
     ]
 )
